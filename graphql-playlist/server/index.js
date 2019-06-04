69b8e8f3eb245bcfc42644406754fb38dea2574f
umed tolibi <tolibiumed@gmail.com>
2019-06-03 23:31:09 -0400

    Initial commit

---
+++ graphql-playlist/server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
+const { username, password } = require("./key");
+const schema = require("./schema/schema");
+
+mongoose
+  .connect(
+    `mongodb://${username}:${password}@ds231377.mlab.com:31377/graphql`,
+    { useNewUrlParser: true },
+    () => console.log("Your database is connected!!!")
+  )
+  .catch(err => console.log("Error: ", err));
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => console.log("Your server is running on port 4000"));
