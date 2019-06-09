5a6564ead8aebd71d5a8342845cb8750434dbd3a
umed tolibi <tolibiumed@gmail.com>
2019-06-09 15:33:15 -0400

    initial commit

--- graphql-playlist/client/src/App.js
+++ graphql-playlist/client/src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
-import BookList from "./components/BookList";
+import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
+// components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+// apollo client setup
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div id="main">
-        <h1>m</h1>
-        <BookList />
-      </div>
-    </ApolloProvider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>My Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
 }
-
 export default App;
--- graphql-playlist/client/src/components/BookList.js
+++ graphql-playlist/client/src/components/BookList.js
@@ -1,35 +1,39 @@
 import React, { Component } from "react";
-import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
+import { getBooksQuery } from "../queries/queries";
 
-const getBooksQuery = gql`
-  {
-    books {
-      name
-      id
-    }
+// components
+import BookDetails from "./BookDetails";
+
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
   }
-`;
-
-class Booklist extends Component {
   displayBooks() {
-    const { data } = this.props;
+    var data = this.props.data;
     if (data.loading) {
-      return <div>Loading Books...</div>;
+      return <div>Loading books...</div>;
     } else {
       return data.books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li key={book.id} onClick={e => this.setState({ selected: book.id })}>
+            {book.name}
+          </li>
+        );
       });
     }
   }
-
   render() {
     return (
       <div>
-        <ul id="book">{this.displayBooks()}</ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
 }
 
-export default graphql(getBooksQuery)(Booklist);
+export default graphql(getBooksQuery)(BookList);
